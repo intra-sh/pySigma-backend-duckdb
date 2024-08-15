aa18d8d6e382c4989771c8725b84a4964352d26b
Patrik Olson (Group) <patrik.olson@security.ntt>
2024-08-15 12:21:02 +0200

    oops

--- sigma/pipelines/duckdb/pipeline.py
+++ sigma/pipelines/duckdb/pipeline.py
@@ -34,7 +34,7 @@ class RuleContainsKeywordCondition(RuleProcessingCondition):
 def duckdb_pipeline() -> ProcessingPipeline:
     return ProcessingPipeline(
         name="DuckDB Pipeline",
-        allowed_backends=frozenset("duckdb"),
+        allowed_backends=frozenset(["duckdb"]),
         priority=50,
         items=[
             ProcessingItem(
